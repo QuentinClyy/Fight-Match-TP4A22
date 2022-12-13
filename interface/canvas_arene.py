611ecf7546d0576a5d53bad0dc0e639445ebaedb
Quentin <96764505+QuentinClyy@users.noreply.github.com>
2022-12-13 16:05:25 -0500

    affichage de avec tkinter commence (challenge)

--- interface/canvas_arene.py
+++ interface/canvas_arene.py
@@ -1,8 +1,5 @@
 from tkinter import Canvas, ALL, LAST
-from texture_loader import GameTextureLoader
-from jeu.arene import Arene
-from jeu.de import De
-import tkinter as tk
+from interface.texture_loader import GameTextureLoader
 
 
 class CanvasArene(Canvas):
@@ -28,12 +25,40 @@ class CanvasArene(Canvas):
         self.textures = GameTextureLoader(self.width, self.height)
         self.des = self.arene.des
 
-    def afficher_de(self):
+    def dessiner_un_de(self, valeur_de):
+        exterieur = (droite - gauche) // 10
+        interieur = (droite - gauche - 4 * exterieur) // 3
+        if valeur_de in ["⚀", "⚂", "⚄"]:
+            self.create_oval(gauche + 2 * exterieur + interieur, haut + 2 * exterieur + interieur,
+                             gauche + 2 * exterieur + 2 * interieur, haut + 2 * exterieur + 2 * interieur,
+                             fill='black')
+        if valeur_de in ["⚁", "⚂", "⚃", "⚄", "⚅"]:
+            self.create_oval(gauche + exterieur, haut + exterieur,
+                             gauche + exterieur + interieur, haut + exterieur + interieur, fill='black')
+            self.create_oval(droite - exterieur, bas - exterieur,
+                             droite - exterieur - interieur, bas - exterieur - interieur, fill='black')
+        if valeur_de in ["⚃", "⚄", "⚅"]:
+            self.create_oval(droite - exterieur, haut + exterieur,
+                             droite - exterieur - interieur, haut + exterieur + interieur, fill='black')
+            self.create_oval(gauche + exterieur, bas - exterieur,
+                             gauche + exterieur + interieur, bas - exterieur - interieur, fill='black')
+        if valeur_de == "⚅":
+            self.create_oval(droite - exterieur, haut + 2 * exterieur + interieur,
+                             droite - exterieur - interieur, haut + 2 * exterieur + 2 * interieur, fill='black')
+            self.create_oval(gauche + exterieur, haut + 2 * exterieur + interieur,
+                             gauche + exterieur + interieur, haut + 2 * exterieur + 2 * interieur, fill='black')
+
+    def dessiner_tous_les_des(self):
+        # valeur_de, x, y = 0, 0, 0
         for coordonnes, de in self.des.items():
             if de is not None:
-                (x, y) = coordonnes
-                valeur_de = de.affichage_de(self.arene.mode_affichage)
-                self.create_image(x, y, image=getattr(self.textures, valeur_de), anchor="center")
+                x, y = coordonnes
+                valeur_de = de.affichage_string(self.arene.mode_affichage)
+                print(valeur_de)
+                if valeur_de in ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]:
+                    pass
+                else:
+                    self.create_image(x, y, image=getattr(self.textures, valeur_de), anchor="center")
 
     def affichage_arene(self, lancer=None):
         self.create_image(0, 0, image=self.textures.arene_bg, anchor="nw")
@@ -49,15 +74,7 @@ class CanvasArene(Canvas):
                 self.create_image(x, y, image=self.textures.arene_tile_dark, anchor="center")
             else:
                 self.create_image(x, y, image=self.textures.arene_tile, anchor="center")
-        self.afficher_de()
+        self.dessiner_tous_les_des()
         self.pack(fill="both", expand=True)
 
 
-if __name__ == "__main__":
-    window = tk.Tk()
-    window.geometry("1920x1080")
-    arene = Arene(7, De(), 2)
-    de_test = De()
-    canvas = CanvasArene(window, arene, 1920, 1080)
-    canvas.affichage_arene()
-    window.mainloop()
--- jeu/de.py
+++ jeu/de.py
@@ -22,24 +22,23 @@ class De:
         """
         self.valeur = None
 
-    def affichage_de(self, mode):
+    def affichage_string(self, mode):
         """
         Donne la représentation en chaîne de caractères de la valeur
-        du dé, selon le mode [2,3,4,5,6] ou [⚁,⚂,⚃,⚄,⚅].
-        Si sa valeur est 1, on affiche X peu importe le mode.
+        du dé, selon le mode [de_1, De_2, de_3, De_4, de_5, De_6] ou [⚁,⚂,⚃,⚄,⚅].
 
         Args:
-            mode (int): Le mode (1 pour [2,3,4,5,6], 2 pour [⚁,⚂,⚃,⚄,⚅]).
+            mode (int): Le mode (1 pour [de_1, De_2, de_3, De_4, de_5, De_6], 2 pour [⚁,⚂,⚃,⚄,⚅]).
 
         Returns:
             str: La représentation de la valeur du dé.
         """
         # VOTRE CODE ICI
         if mode == 1:
-            pass
-        elif mode == 2:
             liste = ["de_1", "de_2", "de_3", "de_4", "de_5", "de_6"]
             return liste[int(self.valeur) - 1]
+        elif mode == 2:
+            return chr(9855 + self.valeur)
 
     def lancer(self):
         """
