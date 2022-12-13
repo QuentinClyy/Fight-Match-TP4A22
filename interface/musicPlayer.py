03a0f8d02ee42c0120d8dbef984cd6a03052bdfe
Quentin <96764505+QuentinClyy@users.noreply.github.com>
2022-12-13 14:36:36 -0500

    reorg

--- interface/musicPlayer.py
+++ interface/musicPlayer.py
@@ -1,6 +1,7 @@
 import pygame
 import tkinter as tk
 
+
 class MusicPlayer:
 
     def __init__(self):
@@ -9,14 +10,7 @@ class MusicPlayer:
 
     def play_music(self):
         a = pygame.mixer.music
-        a.load("./Sounds/startMusic.wav")
-        a.queue("./Sounds/MusiqueLoop.wav", loops=-1)
+        a.load("./interface/Sounds/startMusic.wav")
+        a.queue("./interface/Sounds/MusiqueLoop.wav", loops=-1)
         a.set_volume(0.5)
         a.play()
-
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    music = MusicPlayer()
-    music.play_music()
-    root.mainloop()
